850a2270978820fdcc973aa48ee7cbe9e61ff883
anikatasnin <jarintasnin09@gmail.com>
2025-02-11 17:54:56 +0600

    routing

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+
 const Home = () => {
   return (
     <div>
+      <Header></Header>
       <h2>Home</h2>
+      <Outlet></Outlet>
     </div>
   );
 };
