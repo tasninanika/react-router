e7a68e1de3a67231b167ed7423d14aa42eeeb3c9
anikatasnin <jarintasnin09@gmail.com>
2025-02-11 18:00:17 +0600

    added tailwindcss

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,17 @@
 const Header = () => {
   return (
-    <div>
+    <div className="text-center my-20">
       <h2>Navbar</h2>
       <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact Us</a>
+        <a href="/" className="">
+          Home
+        </a>
+        <a href="/about" className="ms-4">
+          About
+        </a>
+        <a href="/contact" className="ms-4">
+          Contact Us
+        </a>
       </nav>
     </div>
   );
