a0ed95ec4c8eca1a12e99dd13b5c4a8d37af67a5
anikatasnin <jarintasnin09@gmail.com>
2025-02-11 17:45:20 +0600

    added header component

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div>
+      <h2>Navbar</h2>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact Us</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
