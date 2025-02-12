f47967109a2372794762ca8f8b8ac32d02a45c3c
anikatasnin <jarintasnin09@gmail.com>
2025-02-12 09:33:25 +0600

    updated user.jsx

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,17 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
   return (
     <div>
       <h2>OOOOOPPPPPSSSSSSSSSSSSSSSSS</h2>
+      <p>{error.statusText || error.message}</p>
+      {error.status === 404 && (
+        <div>
+          <h3>Page not found</h3>
+          <Link to="/">Go back</Link>
+        </div>
+      )}
     </div>
   );
 };
